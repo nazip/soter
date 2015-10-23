f9b9ad7f753dc294449dc802983a7b1ac630bd4a
nazip <nazipm@mail.ru>
2015-10-23 11:21:56 +0300

    install devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151022121227) do
+ActiveRecord::Schema.define(version: 20151023082035) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -28,6 +28,24 @@ ActiveRecord::Schema.define(version: 20151022121227) do
   end
 
   add_index "products", ["group_id"], name: "index_products_on_group_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "products", "groups"
 end
