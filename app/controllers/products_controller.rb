0b3235ceb752f7b014a337e97d55e36bdd75d22c
nazip <nazipm@mail.ru>
2015-11-13 11:29:24 +0300

    add foto

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,8 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: :index
   # before_action :pandit_authorize, except: [:index, :new]
-  before_action :get_all_products, except: :new
+  before_action :get_all_products, except: [:new, :pictures]
+  before_action :find_product, only: [:pictures, :edit]
 
   # respond_to :js
 
@@ -12,18 +13,34 @@ class ProductsController < ApplicationController
   def sell
   end
 
+  def pictures
+  end
+
   def edit
+    respond_with @product
+  end
+
+  def show
+    redirect_to products_path
   end
 
   def new
-    @product = Product.new
-    # @attachment = @product.attachment.build
+    respond_with(@product = Product.new)
   end
 
   def create
+    respond_with(@product = Product.create(product_params))
   end
 
   private
+
+    def find_product
+      @product = Product.find(params[:id])
+    end
+
+    def product_params
+      params.require(:product).permit(:name, :kolvo, :group_id, attachments_attributes: [:file])
+    end
 
     def get_all_products
       @products = Product.all.order(:group_id)
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
     collection do
       post 'sell'
     end
+    member do
+      get 'pictures'
+    end
     resources :groups
   end
 
